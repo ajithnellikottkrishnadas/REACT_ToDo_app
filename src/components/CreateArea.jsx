49adb680fe88f69c3a4e34b911b4113c991a7052
ajithnellikottkrishnadas <ajithnellikodanofficial@gmail.com>
2025-08-01 17:15:29 +0530

    added note area expanding when click on text area

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Zoom from '@mui/material/Zoom';
+import Fab from '@mui/material/Fab';
 
 function CreateArea(props) {
 
@@ -6,6 +9,8 @@ function CreateArea(props) {
     title: "",
     content: ""
   });
+
+  const [isExpanded,setExpand]=useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -32,21 +37,42 @@ function CreateArea(props) {
       setNote({ title: "", content: "" })
     }
   }
+  function zoomFunction(){
+
+     setExpand(true);
+
+  }
 
 
 
   return (
     <div>
       <div>
-        <form>
-          <input onChange={handleChange} name="title" placeholder="Title" value={note.title} />
-          <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={note.content} />
-          <button onClick={submitNote}  >Add</button>
+        <form onClick={zoomFunction}>
+          {isExpanded==true ? 
+
+          <input 
+          onChange={handleChange} 
+          name="title" 
+          placeholder="Title" 
+          value={note.title} 
+          onClick={zoomFunction}/>    
+           :null }
+
+            <textarea 
+            onChange={handleChange} 
+            name="content" 
+            placeholder="Take a note..." 
+            rows={isExpanded? "3" : "1"} 
+            value={note.content} 
+            />
+          <Zoom in={true}><button><AddIcon/></button></Zoom>
         </form>
       </div>
 
     </div>
   );
+  
 }
 
 export default CreateArea;
